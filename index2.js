536faec5e9063a987cb478af150606bb811bb6a0
lunalo57 <lunalo.dev@gmail.com>
2022-11-11 21:37:04 +0100

    second part

---
+++ index2.js
@@ -0,0 +1,35 @@
+//h GITHUB                                                                  
+
+//g Permet de versionner son travail en plusieurs sauvegardes
+//g à des moments donnés et de pouvoir reprendre des sauvegardes 
+//g antérieures si les updates sont buguées
+
+//p "cd" -> change directory -> permet de se déplacer
+//p "mkdir" -> make directory -> créer un dossier
+//p "touch" -> créer un fichier
+//p "ls" -> list -> affiche la liste des fichiers et dossiers
+//p "cd.." -> permet de revenir en arrière
+//p "code ." -> lance VsCode
+
+//g Quand on versionne un projet, on le fait d'abord en local
+//g puis ensuite on peut le faire en ligne
+
+//y Voici les différentes étapes pour créer un "REPOSITORY"
+//j 1)        " git init "                                                     
+//j 2)        " git status "                                                   
+//j 3)        " git add * "                                                    
+//j 4)        " git commit -m 'first commit' "                                 
+//j 5)        " git remote add origin https://github.com/lunalo57/exemple.git "
+//j 6)        " git branch -M main "                                           
+//j 7)        " git push -u origin main "                                      
+
+//o Pour créer son propre site internet sur Github
+//-> Créer un nouveau repository
+//-> pseudo.github.io -> créer
+//-> on récupère le repository
+//-> on va sur le desktop
+//-> " git clone " -> on clone notre repository dans un dossier
+//-> " git add * "
+//-> " git commit -m 'commit' "
+//-> " git push -u origin main "
+//-> le nom du site sera " https://pseudo.github.io "
